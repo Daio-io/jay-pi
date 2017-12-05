0236d69c4c0f23b4ac3fb9f94a05b34ac0afbf7c
Daio-io <dai.williams@bbc.co.uk>
2017-12-05 17:03:46 +0000

    Update collection name

--- src/main/kotlin/Database.kt
+++ src/main/kotlin/Database.kt
@@ -6,7 +6,7 @@ object Database {
 
     private val connectionString = MongoClientURI(AppConstants.CONNECTION_STRING)
     private val db = MongoClient(connectionString).getDatabase(AppConstants.DATABASE_NAME)
-    private val recipesCollection = db.getCollection("recipes")
+    private val recipesCollection = db.getCollection("jrecipes")
 
     fun getRecipes() = recipesCollection.find().toList()
 
