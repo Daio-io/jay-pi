392b45509ac550e0620a3a6375c71cc89b5a71ba
Daio-io <dai.williams@bbc.co.uk>
2017-12-10 17:53:30 +0000

    Proper failed json res

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,5 +6,5 @@ fun main(args: Array<String>) {
     spark.port(AppConstants.PORT)
     spark.get("/status", function = handlers.statusHandler)
     spark.get("/recipes", function = handlers.recipeHandler)
-    spark.get("/save", function = handlers.saveHandler)
+    spark.post("/save", function = handlers.saveHandler)
 }
